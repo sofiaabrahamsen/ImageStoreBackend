f527c94d480e2fb9a9e9d7e204c1c2b973d8b9a4
sofiaabrahamsen <sofia.abrahamsen@yahoo.com>
2022-11-14 13:56:35 +0100

    second commit

--- src/main/java/com/example/imagestore/controller/imageController.java
+++ src/main/java/com/example/imagestore/controller/imageController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,10 +27,23 @@ public ImageController() {
     return ResponseEntity.ok().body(images);
 }
 
+// OBS: need to talk through it
 @GetMapping("/byuser/{user}") //getmetode til at få hentet ét specifik billede ud fra user
     public ResponseEntity<List<Image>> findByUser(@PathVariable String user){
     List<Image> userImage = images.stream().filter(element -> element.getUser().equals(user)).collect(Collectors.toList());
     System.out.println("Found images:" + userImage.size());
     return ResponseEntity.ok().body(userImage);
 }
+
+@PostMapping() //postmetode to create a image
+    public ResponseEntity<Image> createImage(@RequestBody Image image){
+        image.setId(counter++);
+        image.setCreated(new Date());
+        images.add(image);
+        System.out.println("Added image: ${}");
+        return ResponseEntity.ok().body(image);
+}
+
+@DeleteMapping("/{id}")
+    public ResponseEntity<>
 }
